e6e1546fde0b7832d33f85d8ca2ef0f84fab24ef
cryart <68236130+Gaming552@users.noreply.github.com>
2023-03-25 11:26:00 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lastfm-api-wrapper",
+    version="0.1.0",
+    description="Python wrapper for the Last.fm API",
+    url="https://github.com/Gaming552/lastfm-api-wrapper",
+    author="cryart",
+    author_email="none",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="lastfm api wrapper",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=["requests"],
+    python_requires=">=3.6",
+)
